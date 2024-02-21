c9ecd414fc1766aa53ee54d65c87e9146a4f1cde
SagarREDDY742003 <sagar07042003@gmail.com>
2024-02-21 22:50:20 +0530

    add restaurant and cusines apis

--- ZomApi/api.md
+++ ZomApi/api.md
@@ -3,20 +3,31 @@ API's
 // Page1
 
 # (GET) List of city
+http://localhost:5500/location
 
 # (GET) Rest Wrt City
+http://localhost:5500/restaurants?state_id=1
 
 # (GET) List of Restaurants
+http://localhost:5500/restaurants
 
 # (GET) List of mealType
+http://localhost:5500/mealType
 
 // Page2
 
 # (GET) Restaurants wrt mealType
+http://localhost:5500/restaurants?mealId=2
+http://localhost:5500/restaurants?stateId=2&mealId=2
 
 # (GET) Restaurants wrt mealType + Cuisine
+http://localhost:5500/filter/1?cuisineId=3
 
 # (GET) Restaurants wrt mealType + Cost
+http://localhost:5500/filter/1?lcost=100&hcost=700
+
+# (GET) Restaurants wrt mealType + Cusine + Cost
+http://localhost:5500/filter/1?cusineId=1&lcost=100&hcost=700
 
 // Page3
 
--- ZomApi/app.js
+++ ZomApi/app.js
@@ -24,10 +24,63 @@ app.get('/location',async(req,res) => {
 
 });
 
+// get restaurant details
+app.get('/restaurants',async(req,res) => {
+    let query = {};
+    if(req.query.stateId && req.params.mealId){
+        query = {
+            "state_id":Number(req.query.stateId),
+            "mealTypes.mealtype_id":Number(req.query.mealId)
+        }
+    }
+    else if(req.query.mealId){
+        query = {"mealTypes.mealtype_id":Number(req.query.mealId)}
+    }
+    else if(req.query.stateId){
+        query = {"state_id":Number(req.query.stateId)}
+    }
+    let collection = 'RestaurantData';
+    let output = await getData(collection,query);
+    res.send(output);
+});
+
 // get all meal types
 app.get('/mealType', async(req,res) => {
     let query = {};
     let collection = "mealtype";
+    let output = await getData(collection,query);
+    res.send(output);
+})
+
+app.get('/filter/:mealId', async(req,res) => {
+    let mealId = Number(req.params.mealId);
+    let cuisineId = Number(req.query.cuisineId);
+    let lcost = Number(req.query.lcost);
+    let hcost = Number(req.query.hcost);
+    if(cuisineId){
+        query = {
+            "mealTypes.mealtype_id":Number(mealId),
+            "cuisines.cuisine_id":Number(cuisineId)
+        }
+    }
+    else if(lcost && hcost){
+        query = {
+            "mealTypes.mealtype_id":Number(mealId),
+            $and:[{cost:{$gt:lcost,$lt:hcost}}]
+        }
+    }
+    else if(cuisineId && lcost && hcost){
+        query = {
+            "mealTypes.mealtype_id":Number(mealId),
+            "cuisines.cuisine_id":Number(cuisineId),
+            "mealTypes.mealtype_id":Number(mealId),
+            $and:[{cost:{$gt:lcost,$lt:hcost}}]
+        }
+    }
+    else{
+        query = {}
+    }
+    let collection = 'RestaurantData';
     let output = await getData(collection,query);
     res.send(output);
 })
